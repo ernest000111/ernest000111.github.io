a7726c283b58df1fb6ab16a38d1c8ac42f1675a1
ernest000111 <ernestito120807@gmail.com>
2025-07-14 10:28:09 -0600

    subir website
    he subido todos mis archivos

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+function () {
+    const openButton = document.querySelector('.nav__menu'); 
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+    
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    close.menu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    })
+    
+    
+(){}}
